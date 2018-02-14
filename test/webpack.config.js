6d5206447be5c5ed7aa14bcc2bd72d5322299a6e
china-liji <hello_zaizi@163.com>
2018-02-14 11:06:10 +0800

    增加测试文件

---
+++ test/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	"entry": [
+		"./test/a.js"
+	],
+	"output": {
+		"path": __dirname,
+		"filename": "a.min.js"
+	},
+	"module": {
+		"loaders": [
+			{
+				"test": /\.js?$/,
+				"loader": "./index.js"
+			}
+		]
+	}
+};
